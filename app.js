0424b54c2257bb0e554eaea892687056e370bb90
jim vester <jim@localhost.localdomain>
2020-04-22 07:52:24 -0400

    working demo

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express')
+    , pureApi = require('./index'),
+    bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.json());
+console.log("Server started: http://localhost:1337");
+app.listen(1337);
+
+var options = {
+    debug: true,
+    handlersDir: "./handlers",
+    validateReqFunc: function (req) {
+        var data = req.body || {};
+        return (data["action_str"] !== undefined && typeof data["action_str"] === "string")
+            && (data["data_type"] !== undefined && typeof data["data_type"] === "string")
+            && (data["log_list"] !== undefined && typeof data["log_list"] === "object")
+            && (data["request_map"] !== undefined && typeof data["request_map"] === "object")
+            && (data["trans_map"] !== undefined && typeof data["trans_map"] === "object");
+    },
+    dataType: undefined,
+    actionStr: undefined
+};
+
+app.post("/test", pureApi.endPoint(options));
